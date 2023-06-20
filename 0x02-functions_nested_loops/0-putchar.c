6fa91192eb17b57d047072c5031cbe31369e8940
marienaibo <marietnaibo@gmail.com>
2023-06-20 16:12:14 -0700

    First print

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,23 @@
+#include <stdio.h>
 #include "main.h"
 
-int main() {
-    printf("_putchar\n");
-    return 0;
+/**
+ * main - Print _putchar, followed by a new line.
+ *
+ * Print _putchar without using printf or puts standar input output lib
+ * Or using any secondary function outside main function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char *string = "_putchar\n";
+
+	while (*string)
+	{
+		_putchar(*string++);
+	}
+
+	return (0);
 }
+
