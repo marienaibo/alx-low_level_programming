49f7bf321e7ffd483c67ac7b566f7654f904c6e1
marienaibo <marietnaibo@gmail.com>
2023-06-21 16:30:49 -0700

    a plus b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+/**
+ * add - function that adds two integers and returns the result.
+ * @n1: first number.
+ * @n2: second number.
+ *
+ * Return: Always result.
+ */
+int add(int n1, int n2)
+{
+	int result = n1 + n2;
+
+	return (result);
+}
+
