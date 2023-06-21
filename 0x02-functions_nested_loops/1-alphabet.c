36b5c0d2cf5f2c7e1eb166a7d18f875f06ae425c
marienaibo <marietnaibo@gmail.com>
2023-06-20 18:42:59 -0700

    Alphabet game

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,13 +5,14 @@
  */
 void print_alphabet(void)
 {
-    int i;
-    char alphabet[27] = "abcdefghijklmnopqrstuvwxyz";
+	char letter = 'a';
 
-    for (i = 0; i < 26; i++)
-    {
-        _putchar(alphabet[i]);
-    }
-    _putchar('\n');
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+
+	_putchar('\n');
 }
 
